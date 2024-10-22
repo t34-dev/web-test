3f022296872176406388b83da53864cc69bd602d
solonnocturne <solonnocturne@proton.me>
2024-10-22 23:06:56 +0500

    get tokens

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -9,9 +9,31 @@ import {
   SignInButton,
   UserButton,
 } from '@clerk/clerk-react';
+import { useAuth } from '@clerk/clerk-react';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+function Token() {
+  const [jwt, setJwt] = useState<string | null>(null);
+  const t = useAuth();
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const token = await t.getToken({ template: 'development' });
+      if (!token) {
+        return;
+      }
+      setJwt(token);
+    };
+    fetchToken();
+  }, [t]);
+
+  if (!jwt) {
+    return <p>Loading...</p>; // Пока токен не загружен
+  }
+
+  return <p>{jwt}</p>; // Отображение токена
+}
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
@@ -21,6 +43,7 @@ createRoot(document.getElementById('root')!).render(
       <SignedIn>
         <UserButton />
       </SignedIn>
+      <Token />
       <App />
     </ClerkProvider>
   </StrictMode>,
