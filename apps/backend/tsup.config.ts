548e4990783518ebc43b44de6949ad924d1c9e6f
0xFilosoF <roman.lashnev@mail.ru>
2024-10-27 23:36:40 +0300

    perf: delete skipLibCheck and nodemon

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig, type Options } from 'tsup';
 
 export default defineConfig((options: Options) => ({
   entry: ['src/index.ts'],
-  clean: true,
   format: ['esm'],
   dts: true,
   sourcemap: true,
   external: ['@prisma/client'],
+  clean: true,
   ...options,
 }));
